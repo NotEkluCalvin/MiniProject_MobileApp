5a3140b4a0e7f70fbd07b2767d63bc85fd723c45
NotEkluCalvin <163338264+NotEkluCalvin@users.noreply.github.com>
2024-08-26 11:01:07 +0000

    Created database helper class to manage the database

---
+++ lib/database_helper.dart
@@ -0,0 +1,52 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'users.db');
+    return openDatabase(
+      path,
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE users(id INTEGER PRIMARY KEY, email TEXT, password TEXT)",
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<void> insertUser(String email, String password) async {
+    final db = await database;
+    await db.insert('users', {'email': email, 'password': password});
+  }
+
+  Future<Map<String, dynamic>?> getUser(String email, String password) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'users',
+      where: "email = ? AND password = ?",
+      whereArgs: [email, password],
+    );
+
+    if (maps.isNotEmpty) {
+      return maps.first;
+    }
+    return null;
+  }
+}
